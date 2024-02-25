9920772454caff7cfdcf213b03258559c23038b4
lmatani <lmatarrubia@gmail.com>
2024-02-25 19:33:09 +0100

    Entregable_1 DevelopFlix

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
 import peliculas from './peliculas.js'
+const arrDivGenero = document.querySelectorAll('div.genero');
+
+// incluyo las listas
+for (let divGenero of arrDivGenero){
+    divGenero.appendChild(document.createElement('ul'));
+    for (let pelicula of peliculas){    
+        if (checkPeliEnGenero(divGenero, pelicula))
+        {
+            incluirPeliEnLista(divGenero.id, pelicula);
+        }
+       
+    }
+}
+
+function checkPeliEnGenero(divGenero, pelicula){
+    let arrGenPeli = pelicula.genre_ids.map((elem) => 'genero-' + elem);
+    let checkGen = arrGenPeli.some((elem) => elem === divGenero.id);
+    return checkGen;
+}
+
+function incluirPeliEnLista(idDiv, pelicula){
+    const ulElem = document.querySelector('#' + idDiv + ' ul');
+    let liElem = document.createElement('li');
+    liElem.innerHTML = `
+            <img src="https://image.tmdb.org/t/p/w500/${pelicula.backdrop_path}" alt="${pelicula.title}">
+            <p>${pelicula.title}</p>`;
+    ulElem.appendChild(liElem);
+}
 
